cd857e19df1aadab01950311153469d3f704ef7a
yuma0306 <yuma0306895@gmail.com>
2024-11-10 11:13:15 +0700

    clean test file

--- e2e/test-1.spec.ts
+++ e2e/test-1.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('show modal', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('button', { name: '関東' }).click();
   await expect(page.getByText('草津温泉伊香保温泉有馬温泉')).toBeVisible();
